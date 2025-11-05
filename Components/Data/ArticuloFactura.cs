a8a5f16e718211283a46b7c0c15759ac8f1fae4b
emilio_com <jorgeemilio7777@gmail.com>
2025-11-05 13:37:21 -0700

    Se agregaron los constructores

---
+++ Components/Data/ArticuloFactura.cs
@@ -0,0 +1,12 @@
+﻿namespace blazerFacturacion.Components.Data
+{
+    public class ArticuloFactura
+    {
+        public int Id { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+        public decimal Total => Cantidad * PrecioUnitario;
+
+    }
+}
---
+++ Components/Data/Factura.cs
@@ -0,0 +1,11 @@
+﻿namespace blazerFacturacion.Components.Data
+{
+    public class Factura
+    {
+        public int Id { get; set; }
+        public string Cliente { get; set; } = string.Empty;
+        public DateTime Fecha { get; set; }
+        public List<ArticuloFactura> Articulos { get; set; } = new List<ArticuloFactura>();
+        public decimal Total => Articulos.Sum(a => a.Total);
+    }
+}
