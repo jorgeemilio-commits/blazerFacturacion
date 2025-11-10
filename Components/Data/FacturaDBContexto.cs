4599a9ec3afa65b4268cf87e7ec767a1faac2c6e
emilio_com <jorgeemilio7777@gmail.com>
2025-11-09 19:36:50 -0700

    Se creo facturaDBcontexto que inicializa la base de datos

---
+++ Components/Data/FacturaDBContexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace blazerFacturacion.Components.Data
+{
+    public class FacturaDbContexto : DbContext
+    {
+        public FacturaDbContexto(DbContextOptions<FacturaDbContexto> options)
+            : base(options)
+        {
+        }
+
+        // tablas en la base de datos
+        public DbSet<Factura> Facturas { get; set; }
+        public DbSet<ArticuloFactura> ArticulosFactura { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
