c71ede32bbb1a58f160ec432b1b9358c42716b1b
emilio_com <jorgeemilio7777@gmail.com>
2025-11-12 13:12:41 -0700

    Se agrego la columnsa de fecha

--- Components/Data/ArticuloFactura.cs
+++ Components/Data/ArticuloFactura.cs
@@ -6,12 +6,12 @@ namespace blazerFacturacion.Components.Data
     {
         [Key]
         public Guid ArticuloId { get; set; } = Guid.NewGuid();
-
         public string NombreCliente { get; set; } = string.Empty;
-
         public string Nombre { get; set; } = string.Empty;
         public int Cantidad { get; set; } = 1;
         public decimal PrecioUnitario { get; set; } = 0;
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
 
         public decimal Total { get { return Cantidad * PrecioUnitario; } }
     }
