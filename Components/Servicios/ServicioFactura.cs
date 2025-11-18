c95dd9f40d453a84404e09c49479d58779a07c14
emilio_com <jorgeemilio7777@gmail.com>
2025-11-18 13:40:26 -0700

    Se crearon nuevas funciones para manipular los articulos

--- Components/Servicios/ServicioFactura.cs
+++ Components/Servicios/ServicioFactura.cs
@@ -3,16 +3,20 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
 
 namespace blazerFacturacion.Components.Servicios
 {
     public class ServicioFactura
     {
         private readonly FacturaDbContexto _contexto;
+        private readonly string _cadenaConexion;
 
-        public ServicioFactura(FacturaDbContexto contexto)
+        public ServicioFactura(FacturaDbContexto contexto, IConfiguration configuracion)
         {
             _contexto = contexto;
+            _cadenaConexion = configuracion.GetConnectionString("DefaultConnnection");
         }
 
         // Guarda una factura
@@ -91,6 +95,68 @@ namespace blazerFacturacion.Components.Servicios
                             f.Fecha.Year == anio)
                 .OrderBy(f => f.Fecha) // Ordenar por fecha
                 .ToListAsync();
+        }
+
+        // Obtener el catálogo de artículos
+        public async Task<List<Articulo>> GetCatalogoArticulosAsync()
+        {
+            var listaArticulos = new List<Articulo>();
+
+            using (var conexion = new SqliteConnection(_cadenaConexion))
+            {
+                await conexion.OpenAsync();
+
+                var consulta = "SELECT Id, Nombre, Precio FROM Articulos";
+
+                using (var comando = new SqliteCommand(consulta, conexion))
+                {
+                    using (var lector = await comando.ExecuteReaderAsync())
+                    {
+                        while (await lector.ReadAsync())
+                        {
+                            listaArticulos.Add(new Articulo
+                            {
+                                Id = lector.GetInt32(0),      // Columna 0
+                                Nombre = lector.GetString(1), // Columna 1
+                                Precio = lector.GetDecimal(2) // Columna 2
+                            });
+                        }
+                    }
+                }
+            }
+            return listaArticulos;
+        }
+
+        // Guardar o actualizar un artículo en el catálogo
+        public async Task GuardarArticuloCatalogoAsync(Articulo articulo)
+        {
+            using (var conexion = new SqliteConnection(_cadenaConexion))
+            {
+                await conexion.OpenAsync();
+                string sentencia;
+
+                if (articulo.Id == 0)
+                {
+                    sentencia = "INSERT INTO Articulos (Nombre, Precio) VALUES (@nombre, @precio)";
+                }
+                else
+                {
+                    sentencia = "UPDATE Articulos SET Nombre = @nombre, Precio = @precio WHERE Id = @id";
+                }
+
+                using (var comando = new SqliteCommand(sentencia, conexion))
+                {
+                    comando.Parameters.AddWithValue("@nombre", articulo.Nombre);
+                    comando.Parameters.AddWithValue("@precio", articulo.Precio);
+
+                    if (articulo.Id != 0)
+                    {
+                        comando.Parameters.AddWithValue("@id", articulo.Id);
+                    }
+
+                    await comando.ExecuteNonQueryAsync();
+                }
+            }
         }
     }
 }
