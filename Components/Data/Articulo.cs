dac45febc32714c4ddb9cc9ebf2c68798fe16288
emilio_com <jorgeemilio7777@gmail.com>
2025-11-18 13:33:29 -0700

    Se creo el constructor de articulos para guardar informacion de los articulos individualmente

---
+++ Components/Data/Articulo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blazerFacturacion.Components.Data
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+        public decimal Precio { get; set; }
+
+    }
+}
