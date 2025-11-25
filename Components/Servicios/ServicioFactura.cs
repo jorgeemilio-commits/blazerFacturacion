44b53706ccc0d7045296b37e0d8fd9ba76487cc2
emilio_com <jorgeemilio7777@gmail.com>
2025-11-25 07:45:06 -0700

    Se agrego la funcion de poder editar los articulos de las facturas despues de ser creadas

--- Components/Servicios/ServicioFactura.cs
+++ Components/Servicios/ServicioFactura.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.Configuration;
-using System; 
+using System;
+using System.Linq;
 
 namespace blazerFacturacion.Components.Servicios
 {
@@ -16,14 +17,13 @@ namespace blazerFacturacion.Components.Servicios
             _cadenaConexion = configuracion.GetConnectionString("DefaultConnection");
         }
 
-        // Guarda una factura (INSERT)
+        // --- MÉTODOS EXISTENTES (Guardar, Leer, Eliminar Factura Completa) ---
+
         public async Task GuardarFacturaAsync(Factura nuevaFactura)
         {
             using (var conexion = new SqliteConnection(_cadenaConexion))
             {
                 await conexion.OpenAsync();
-
-                // Inserta la "Cabecera" de la factura
                 var sqlFactura = @"
                     INSERT INTO Facturas (NombreCliente, Fecha, Total) 
                     VALUES (@nombre, @fecha, @total);
@@ -37,12 +37,10 @@ namespace blazerFacturacion.Components.Servicios
                     comando.Parameters.AddWithValue("@fecha", nuevaFactura.Fecha.ToString("yyyy-MM-dd"));
                     comando.Parameters.AddWithValue("@total", nuevaFactura.TotalCalculado);
 
-                    // Ejecuta la consulta y devuelve el primer valor (el ID)
                     var resultado = await comando.ExecuteScalarAsync();
                     idFacturaGenerado = Convert.ToInt32(resultado);
                 }
 
-                // Inserta los "Artículos" de la factura
                 foreach (var articulo in nuevaFactura.Articulos)
                 {
                     var sqlArticulo = @"
@@ -55,25 +53,20 @@ namespace blazerFacturacion.Components.Servicios
                         comandoArt.Parameters.AddWithValue("@cant", articulo.Cantidad);
                         comandoArt.Parameters.AddWithValue("@precio", articulo.PrecioUnitario);
                         comandoArt.Parameters.AddWithValue("@idFactura", idFacturaGenerado);
-
                         await comandoArt.ExecuteNonQueryAsync();
                     }
                 }
             }
         }
 
-        // Ver facturas por nombre de cliente (SELECT)
         public async Task<List<Factura>> GetFacturasPorClienteAsync(string nombreCliente)
         {
             var listaFacturas = new List<Factura>();
-
             if (string.IsNullOrWhiteSpace(nombreCliente)) return listaFacturas;
 
             using (var conexion = new SqliteConnection(_cadenaConexion))
             {
                 await conexion.OpenAsync();
-
-                // Buscamos las facturas (Cabeceras)
                 var sql = @"SELECT Id, NombreCliente, Fecha, Total 
                             FROM Facturas 
                             WHERE UPPER(NombreCliente) = UPPER(@nombre) 
@@ -82,28 +75,25 @@ namespace blazerFacturacion.Components.Servicios
                 using (var comando = new SqliteCommand(sql, conexion))
                 {
                     comando.Parameters.AddWithValue("@nombre", nombreCliente);
-
                     using (var lector = await comando.ExecuteReaderAsync())
                     {
                         while (await lector.ReadAsync())
                         {
-                            var factura = new Factura
+                            listaFacturas.Add(new Factura
                             {
                                 Id = lector.GetInt32(0),
                                 NombreCliente = lector.GetString(1),
                                 Fecha = DateTime.Parse(lector.GetString(2)),
                                 Total = lector.GetDecimal(3),
-                                Articulos = new List<ArticuloFactura>() // Inicializamos lista vacía
-                            };
-                            listaFacturas.Add(factura);
+                                Articulos = new List<ArticuloFactura>()
+                            });
                         }
                     }
                 }
 
-                // Carga los artículos para cada factura encontrada
                 foreach (var factura in listaFacturas)
                 {
-                    var sqlArticulos = @"SELECT Nombre, Cantidad, PrecioUnitario 
+                    var sqlArticulos = @"SELECT ArticuloId, Nombre, Cantidad, PrecioUnitario, FacturaId 
                                          FROM ArticulosFactura 
                                          WHERE FacturaId = @id";
 
@@ -116,9 +106,11 @@ namespace blazerFacturacion.Components.Servicios
                             {
                                 factura.Articulos.Add(new ArticuloFactura
                                 {
-                                    Nombre = lectorArt.GetString(0),
-                                    Cantidad = lectorArt.GetInt32(1),
-                                    PrecioUnitario = lectorArt.GetDecimal(2)
+                                    ArticuloId = lectorArt.GetInt32(0),
+                                    Nombre = lectorArt.GetString(1),
+                                    Cantidad = lectorArt.GetInt32(2),
+                                    PrecioUnitario = lectorArt.GetDecimal(3),
+                                    FacturaId = lectorArt.GetInt32(4)
                                 });
                             }
                         }
@@ -128,39 +120,33 @@ namespace blazerFacturacion.Components.Servicios
             return listaFacturas;
         }
 
-        // Actualizar factura (UPDATE)
         public async Task ActualizarFacturaAsync(int idFactura, string nuevoNombre, DateTime nuevaFecha)
         {
             using (var conexion = new SqliteConnection(_cadenaConexion))
             {
                 await conexion.OpenAsync();
                 var sql = "UPDATE Facturas SET NombreCliente = @nombre, Fecha = @fecha WHERE Id = @id";
-
                 using (var comando = new SqliteCommand(sql, conexion))
                 {
                     comando.Parameters.AddWithValue("@nombre", nuevoNombre);
                     comando.Parameters.AddWithValue("@fecha", nuevaFecha.ToString("yyyy-MM-dd"));
                     comando.Parameters.AddWithValue("@id", idFactura);
-
                     await comando.ExecuteNonQueryAsync();
                 }
             }
         }
 
-        // Eliminar factura (DELETE)
         public async Task EliminarFacturaAsync(int idFactura)
         {
             using (var conexion = new SqliteConnection(_cadenaConexion))
             {
                 await conexion.OpenAsync();
-
                 var sqlBorrarHijos = "DELETE FROM ArticulosFactura WHERE FacturaId = @id";
                 using (var cmdHijos = new SqliteCommand(sqlBorrarHijos, conexion))
                 {
                     cmdHijos.Parameters.AddWithValue("@id", idFactura);
                     await cmdHijos.ExecuteNonQueryAsync();
                 }
-
                 var sqlBorrarPadre = "DELETE FROM Facturas WHERE Id = @id";
                 using (var cmdPadre = new SqliteCommand(sqlBorrarPadre, conexion))
                 {
@@ -170,7 +156,6 @@ namespace blazerFacturacion.Components.Servicios
             }
         }
 
-        // Reporte anual (SELECT con filtros de fecha)
         public async Task<List<Factura>> GetFacturasPorAnioAsync(string nombreCliente, int anio)
         {
             var lista = new List<Factura>();
@@ -179,7 +164,6 @@ namespace blazerFacturacion.Components.Servicios
             using (var conexion = new SqliteConnection(_cadenaConexion))
             {
                 await conexion.OpenAsync();
-
                 var sql = @"SELECT Id, NombreCliente, Fecha, Total 
                             FROM Facturas 
                             WHERE UPPER(NombreCliente) = UPPER(@nombre) 
@@ -190,7 +174,6 @@ namespace blazerFacturacion.Components.Servicios
                 {
                     comando.Parameters.AddWithValue("@nombre", nombreCliente);
                     comando.Parameters.AddWithValue("@anio", anio.ToString());
-
                     using (var lector = await comando.ExecuteReaderAsync())
                     {
                         while (await lector.ReadAsync())
@@ -207,6 +190,86 @@ namespace blazerFacturacion.Components.Servicios
                 }
             }
             return lista;
+        }
+
+        // --- NUEVOS MÉTODOS PARA EDITAR ARTÍCULOS ---
+
+        public async Task AgregarArticuloAsync(ArticuloFactura articulo)
+        {
+            using (var conexion = new SqliteConnection(_cadenaConexion))
+            {
+                await conexion.OpenAsync();
+                var sql = @"INSERT INTO ArticulosFactura (Nombre, Cantidad, PrecioUnitario, FacturaId) 
+                            VALUES (@nombre, @cant, @precio, @idFactura)";
+
+                using (var comando = new SqliteCommand(sql, conexion))
+                {
+                    comando.Parameters.AddWithValue("@nombre", articulo.Nombre);
+                    comando.Parameters.AddWithValue("@cant", articulo.Cantidad);
+                    comando.Parameters.AddWithValue("@precio", articulo.PrecioUnitario);
+                    comando.Parameters.AddWithValue("@idFactura", articulo.FacturaId);
+                    await comando.ExecuteNonQueryAsync();
+                }
+            }
+            await RecalcularTotalFacturaAsync(articulo.FacturaId);
+        }
+
+        public async Task ActualizarArticuloAsync(ArticuloFactura articulo)
+        {
+            using (var conexion = new SqliteConnection(_cadenaConexion))
+            {
+                await conexion.OpenAsync();
+                var sql = @"UPDATE ArticulosFactura 
+                            SET Nombre = @nombre, Cantidad = @cant, PrecioUnitario = @precio 
+                            WHERE ArticuloId = @id";
+
+                using (var comando = new SqliteCommand(sql, conexion))
+                {
+                    comando.Parameters.AddWithValue("@nombre", articulo.Nombre);
+                    comando.Parameters.AddWithValue("@cant", articulo.Cantidad);
+                    comando.Parameters.AddWithValue("@precio", articulo.PrecioUnitario);
+                    comando.Parameters.AddWithValue("@id", articulo.ArticuloId);
+                    await comando.ExecuteNonQueryAsync();
+                }
+            }
+            await RecalcularTotalFacturaAsync(articulo.FacturaId);
+        }
+
+        public async Task EliminarArticuloAsync(int idArticulo, int idFactura)
+        {
+            using (var conexion = new SqliteConnection(_cadenaConexion))
+            {
+                await conexion.OpenAsync();
+                var sql = "DELETE FROM ArticulosFactura WHERE ArticuloId = @id";
+                using (var comando = new SqliteCommand(sql, conexion))
+                {
+                    comando.Parameters.AddWithValue("@id", idArticulo);
+                    await comando.ExecuteNonQueryAsync();
+                }
+            }
+            await RecalcularTotalFacturaAsync(idFactura);
+        }
+
+        private async Task RecalcularTotalFacturaAsync(int idFactura)
+        {
+            using (var conexion = new SqliteConnection(_cadenaConexion))
+            {
+                await conexion.OpenAsync();
+                // Calcula el nuevo total sumando los artículos existentes
+                var sql = @"UPDATE Facturas 
+                            SET Total = (
+                                SELECT COALESCE(SUM(Cantidad * PrecioUnitario), 0) 
+                                FROM ArticulosFactura 
+                                WHERE FacturaId = @id
+                            )
+                            WHERE Id = @id";
+
+                using (var comando = new SqliteCommand(sql, conexion))
+                {
+                    comando.Parameters.AddWithValue("@id", idFactura);
+                    await comando.ExecuteNonQueryAsync();
+                }
+            }
         }
     }
 }
